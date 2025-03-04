c9bff00ec8ee0db2f661f493b8f8e37de09db229
ducable16 <ductrant1k29@gmail.com>
2025-03-04 11:18:04 +0700

    test ci 11

--- gateway/src/main/java/com/example/Request.java
+++ gateway/src/main/java/com/example/Request.java
@@ -13,21 +13,7 @@ public class Request {
     public String getMethod() {
         return method;
     }
-    public void setMethod(String method) {
-        this.method = method;
-    }
-    public String getUrl() {
-        return url;
-    }
-    public void setUrl(String url) {
-        this.url = url;
-    }
-    public String getBody() {
-        return body;
-    }
-    public void setBody(String body) {
-        this.body = body;
-    }
+
 
 
 }
